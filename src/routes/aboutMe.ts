05bb6173b50408113c52134ee1f1bad730caac5f
Zachary Muzzleman <zach12342@me.com>
2024-11-20 19:52:02 -0500

    Added routes for each archor link when scrolling the page. Removes the hash from the link in the url as well

--- src/routes/aboutMe.ts
+++ src/routes/aboutMe.ts
@@ -3,6 +3,10 @@ import path from 'path';
 
 export const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req,res) => {
+    res.redirect('/#about-me');
+});
+
+router.get('/#about-me', (req,res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
+});
--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -3,6 +3,10 @@ import path from 'path';
 
 export const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req,res) => {
+    res.redirect('/#projects');
+});
+
+router.get('/#projects', (req,res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
+})
